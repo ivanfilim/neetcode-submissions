2308de98bdd3f5d2743b6604fb44ec91f6dd4635
Ivan Filimonov <ivanf20058@gmail.com>
2026-07-15 23:22:10 +0200

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i in range(len(nums)):
+            d[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in d and d[complement] != i:
+                return [i, d[complement]]
+        raise RuntimeError("Invalid input")
