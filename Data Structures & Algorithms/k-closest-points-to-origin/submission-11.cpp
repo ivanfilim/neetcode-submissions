e0c387ae84237affbe666736a32733fc5275c29a
Ivan Filimonov <ivanf20058@gmail.com>
2026-08-30 19:14:40 +0200

    Add: k-closest-points-to-origin - submission-11

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-11.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, vector<int>>> heap;
+        for (const auto& point : points) {
+            int distance = point[0] * point[0] + point[1] * point[1];
+            heap.push({distance, point});
+            if (heap.size() > k) heap.pop();
+        }
+        vector<vector<int>> res;
+        while (!heap.empty()) {
+            vector<int> point = heap.top().second;
+            res.push_back(point);
+            heap.pop();
+        }
+        return res;
+    }
+};
